e8699d40dacc007352a94e730965f914aa0bf316
SantoshShah <santoshshah010@gmail.com>
2015-01-07 19:53:55 +0100

    A type provider 3.0 example with OData as data source

---
+++ OData/OData/Program.fs
@@ -0,0 +1,20 @@
+﻿//Example chosen from http://msdn.microsoft.com/en-us/library/hh156504.aspx
+
+open Microsoft.FSharp.Data.TypeProviders
+
+//source
+type ODataSource = ODataService<"http://services.odata.org/Northwind/Northwind.svc/">
+
+//creating objects for data
+let dataobject = ODataSource.GetDataContext()
+let dataContext = ODataSource.ServiceTypes.NorthwindEntities()
+
+//Querying Odata using F# query expression
+query { for employer in dataobject.Employees do
+        select employer }
+|> Seq.iter (fun employer ->
+    printfn "Firstname: %s" employer.FirstName
+    )
+
+//so that console doesn't close abruptly
+let x = System.Console.ReadLine();
