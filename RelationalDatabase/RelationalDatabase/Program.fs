149a5e173c34f71fefb2d5d5eb8b88363e695fa0
SantoshShah <santoshshah010@gmail.com>
2015-01-07 19:54:53 +0100

    A type provider 3.0 example with relational database as example

---
+++ RelationalDatabase/RelationalDatabase/Program.fs
@@ -0,0 +1,32 @@
+﻿module internal RelationalDatabase
+
+open System.Data.Linq
+open System.Data.Entity
+open Microsoft.FSharp.Data.TypeProviders
+
+ type private EntityConnection = SqlEntityConnection<ConnectionString=" Server=(localdb)\mssqllocaldb;
+ Initial Catalog=UNI-KL;Integrated Security=SSPI;MultipleActiveResultSets=true",Pluralize = true>
+
+ let private context = EntityConnection.GetDataContext()
+
+[<EntryPoint>]
+//displays seminar title
+ query { for seminar in context.Seminars do
+        select seminar }
+|> Seq.iter (fun seminar -> printfn "%s" seminar.Title)
+
+//joins the table
+query { for seminar in context.Seminars do
+        join Participant in context.Participants on (seminar.SeminarID = Participant.SeminarID)
+        select (seminar.SeminarID,seminar.Level,Participant.Name, Participant.Grade) }
+|> Seq.iter (fun (SeminarID, Level, Name, Grade) -> printfn "%d %s %s %s" SeminarID Level Name Grade)
+
+
+//use of where clause to filter
+query { for seminar in context.Seminars do
+        where (seminar.Level= "Bachelor")
+        select seminar }
+|> Seq.iter (fun seminar -> printfn "%s" seminar.Title)
+
+//console doesn't close abruptly
+let x = System.Console.ReadLine();
